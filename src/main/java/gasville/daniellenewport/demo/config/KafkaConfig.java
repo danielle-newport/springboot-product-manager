c7d1bf7f8ee06a3f8b1fbb07634990207f211437
danielle-newport <daniellenewport98@gmail.com>
2025-05-05 16:55:30 -0500

    Add missing import

--- src/main/java/gasville/daniellenewport/demo/config/KafkaConfig.java
+++ src/main/java/gasville/daniellenewport/demo/config/KafkaConfig.java
@@ -1,6 +1,7 @@
 // config/KafkaConfig.java
 package main.java.gasville.daniellenewport.demo.config;
 
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Value;
